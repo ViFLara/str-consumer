d3511a73e13655425d5efc21dc1a58b7693b9b3e
Viviane Lara <viviflara@gmail.com>
2023-05-22 01:43:33 -0300

    record interceptor

--- src/main/java/com/kafka/strconsumer/listeners/StrConsumerListener.java
+++ src/main/java/com/kafka/strconsumer/listeners/StrConsumerListener.java
@@ -3,7 +3,6 @@ package com.kafka.strconsumer.listeners;
 import com.kafka.strconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
@@ -20,7 +19,7 @@ public class StrConsumerListener {
         log.info("LOG ::: Receive message {}", message);
     }
 
-    @StrConsumerCustomListener(groupId = "group-2")
+    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "validMessageContainerFactory")
     public void history(String message) {
         log.info("HISTORY ::: Receive message {}", message);
     }
